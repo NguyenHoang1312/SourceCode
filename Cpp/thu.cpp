b00b653f7d0407471e0ae4129462f61ab82687a7
NguyenHoang1312 <hoangfnaf@gmail.com>
2023-12-23 01:22:15 +0700

    Change thu.cpp

--- Cpp/thu.cpp
+++ Cpp/thu.cpp
@@ -1,36 +1,6 @@
-#include<bits/stdc++.h>
-
-class HINH {
-protected:
-    float dai, rong;
-public:
-    void nhap() {
-        do {
-            std::cin >> dai >> rong;
-        } while (dai <= 0 || rong <= 0);
-    }
-    void xuat() {
-        std::cout << dai << " " << rong << std::endl;
-    }
-};
-
-class HCN: public HINH {
-public:
-    void xuat() {
-        std::cout << chuVi() << " " << dienTich() << std::endl;
-    }
-    float chuVi() {
-        return (dai + rong) * 2;
-    }
-    float dienTich() {
-        return dai * rong;
-    }
-};
+#include<iostream>
+using namespace std;
 
 int main() {
-    HCN a;
-    a.nhap();
-    a.xuat();
-    std::cout << a.chuVi() << std::endl;
-    std::cout << a.dienTich() << std::endl;
+    cout << "Hoang";
 }
