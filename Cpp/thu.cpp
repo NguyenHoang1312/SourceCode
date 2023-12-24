c5555a655ef9089a498825a52193b48e5c48d3de
hwanguyn <127913818+NguyenHoang1312@users.noreply.github.com>
2023-12-24 09:26:52 +0700

    Update thu.cpp

--- Cpp/thu.cpp
+++ Cpp/thu.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
-using namespace std;
+#include<bits/stdc++.h>
+
+bool isPrime(int number) {
+    for (int i = 2; i*i <= number; ++i)
+        if (number % i == 0) return false;
+    return number > 1;
+}
 
 int main() {
-    cout << "Hoang";
+    int n;
+    std::cout << "Nhap so tu nhien n: ";
+    std::cin >> n;
+    std::cout << (isPrime(n)?:"La so nguyen to!":"Khong phai so nguyen to!");
 }
