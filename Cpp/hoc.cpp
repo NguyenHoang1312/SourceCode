1e61cd3f1480f36c4cec53a2224fd18af25cd9c1
NguyenHoang1312 <hoangfnaf@gmail.com>
2024-01-01 08:14:58 +0700

    Update

--- Cpp/hoc.cpp
+++ Cpp/hoc.cpp
@@ -1,30 +1,49 @@
-#include<iostream>
-#include<queue>
-#include<string>
+#include<bits/stdc++.h>
+// using namespace std;
+
+// double f(double x) {
+//     return x*x*x + x - 5;
+// }
+double f(double x) {
+    return pow(x, 4) - 4*x - 1;
+}
+
+double dayCung(double a, double b) {
+    double x, e = 0.001;
+    do {
+        x = a - ((b-a)*f(a))/(f(b) - f(a));
+        if (f(x)*f(b) < 0) a = x;
+        else if (f(x)*f(a) < 0) b = x;
+    } while (fabs(b-a) > e);
+    return x;
+}
+
+// int main() {
+//     cout << dayCung(1, 2);
+// }
+
+// #include <iostream>
+// #include <cmath>
+
+
+
+double chord_method(double a, double b, double epsilon) {
+    if (f(a)*f(b) >= 0) {
+        std::cout << "Phương pháp dây cung không hội tụ." << std::endl;
+        return 0;
+    }
+    if (fabs(f(a)) < fabs(f(b))) {
+        std::swap(a, b);
+    }
+    while (fabs(b-a) >= epsilon) {
+        double temp = b;
+        b = b - (b-a)*f(b)/(f(b)-f(a));
+        a = temp;
+    }
+    return b;
+}
 
 int main() {
-    int num;
-    std::cout << "Input n = ";
-    std::cin >> num;
-    
-    std::queue<std::string> binary;
-    std::vector<std::string> result;
-    binary.push("0");
-    binary.push("1");
-    result.push_back("0");
-    result.push_back("1");
-
-    while (true) {
-        std::string top = binary.front();
-        binary.pop();
-        if (top.size() == num+1) break;
-        binary.push(top + "0");
-        binary.push(top + "1");
-        result.push_back(top + "0");
-        result.push_back(top + "1");
-    }
-
-    std::cout << "Result:\n";
-    for (std::string it: result)
-        if (it.size() == num) std::cout << it << std::endl;
+    std::cout << dayCung(0, 2) << std::endl;
+    return 0;
 }
