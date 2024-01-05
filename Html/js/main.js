c72a278e27d425698031f3b5e14f674d0726e926
NguyenHoang1312 <hoangfnaf@gmail.com>
2024-01-05 20:06:36 +0700

    Updated

--- Html/js/main.js
+++ Html/js/main.js
@@ -1,60 +1,13 @@
-var courses = [
-    {name: 'javascript', coin: 680, isFinish: true},
-    {name: 'php', coin: 860, isFinish: true},
-    {name: 'ruby', coin: 980, isFinish: true}
-];
+/**
+ * Sử dụng các biến đã cho sẵn dưới đây
+ */
 
-//myFilter
-Array.prototype.myFilter = function(callback) {
-    let output = [];
-    for (let index in this) {
-        if (this.hasOwnProperty(index)) {
-            if (callback(this[index], index, this)) {
-                output.push(this[index]);
-            }
-        }
-    }
-    return output;
-}
+var productsListElement = document.getElementsByClassName('products-list');
+var firstProductElement = document.querySelectorAll('.products-list:first-child');
+var buttonElements = document.getElementsByTagName('button');
 
-var fitlerCourses = courses.myFilter(function(course) {
-    return course.coin > 700;
-});
+console.log(productsListElement);
+console.log(firstProductElement);
+console.log(buttonElements);
 
-console.log(fitlerCourses);
 
-//mySome
-Array.prototype.mySome = function(callback) {
-    for (let index in this) {
-        if (this.hasOwnProperty(index)) {
-            if (callback(this[index], index, this)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-var someCourses = courses.mySome(function(course) {
-    return course.coin > 900;
-});
-
-console.log(someCourses);
-
-//myEvery
-Array.prototype.myEvery = function(callback) {
-    for (let index in this) {
-        if (this.hasOwnProperty(index)) {
-            if (!callback(this[index], index, this)) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-var everyCourses = courses.myEvery(function(course) {
-    return course.isFinish === true;
-});
-
-console.log(everyCourses);
