6e52f39ecf4a0a59bd2449db2e2d5611b528ad8c
NguyenHoang1312 <hoangfnaf@gmail.com>
2024-01-07 16:06:59 +0700

    Updated

--- Cpp/hoc.cpp
+++ Cpp/hoc.cpp
@@ -3,20 +3,31 @@
 #define ll long long
 using namespace std;
 
+bool isPrime(ll number) {
+    for (ll i = 2; i*i <= number; ++i)
+        if (number % i == 0) return false;
+    return number > 1;
+} 
+
 int main() {
-    ios 
-    ll *a, n, k;
-    cin >> n >> k;
-    a = new ll[n];
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    ll p = 0 , q = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (a[i] + a[j] == 2*k) {
-                p = a[i]; q = a[j];
-                break;
-            }
-        }
+    // ios 
+    ll n, m, u, v, sum;
+    cin >> n >> m;
+
+    ll a[n], b[n];
+    memset(b, 0, sizeof(b));
+
+    for (ll i = 0; i < n; ++i)
+        cin >> a[i];
+
+    b[0] = a[0];
+    for (ll i = 1; i < n; ++i)
+        b[i] = b[i-1] + a[i];
+
+    while (m--) {
+        cin >> u >> v;
+        sum = b[v-1] - b[u-1] + a[u-1];
+        if (isPrime(sum)) cout << "1\n";
+        else cout << "0\n";
     }
-    cout << p << " " << q;
 }
