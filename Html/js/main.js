213aa8f68ee4e5b9e95fdaa9431b45dc57d7bbac
NguyenHoang1312 <hoangfnaf@gmail.com>
2024-01-17 00:42:21 +0700

    Updated

--- Html/js/main.js
+++ Html/js/main.js
@@ -1,21 +1,7 @@
-var a = document.querySelector('.box');
+var a = [1, 2, 3, [4, 5], 6, [7, 8, 9]];
 
-a.innerHTML = '<h1 style="color: red; class="">HwangNguyen</h1>'; 
+var b = a.reduce((list, child) => {
+    return list.concat(child);
+}, []);
 
-a.style = 'border: solid green; width: 210px; height: 100px';
-
-setInterval(()=>{
-    a.classList.toggle('red');
-    let time = new Date();
-    let hour = String(time.getHours()).padStart(2, '0');
-    let minute = String(time.getMinutes()).padStart(2, '0');
-    let second = String(time.getSeconds()).padStart(2, '0');
-    let date = String(time.getDate()).padStart(2, '0');
-    let month = String(time.getMonth()+1).padStart(2, '0');
-    let year = String(time.getFullYear()).padStart(2, '0');
-
-    let newExperience = document.createElement('h1');
-    newExperience.innerHTML = `<h1>${date}/${month}/${year}</h1>`;
-    a.innerHTML = '<h1>' + hour+':'+minute+':'+second + '</h1>';
-    a.appendChild(newExperience);
-}, 1000);
+console.log(b);
---
+++ Python/hoc.py
@@ -0,0 +1,6 @@
+a, b, c = 1, 3, [4, 5]
+
+print(a)
+print(b)
+print(c)
+
