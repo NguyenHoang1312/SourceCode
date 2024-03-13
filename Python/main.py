4c61b6fbabf5fbab0f1eb0a14377d68afdd312f0
NguyenHoang1312 <hoangfnaf@gmail.com>
2024-03-13 21:54:22 +0700

    Updated

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,3 @@
-def max(a, b, c):
-    mx = a
-    if mx < b: mx = b 
-    if mx < c: mx = c
-    return mx
+names = ['Amir', 'Bear', 'Charlton', 'Daman']
 
-a, b, c, d, e = map(int, input().split())
-max1 = max(a, b, c)
-max2 = max(max1, d, e)
-
-print(max2)
+print(names[-1][-1])
